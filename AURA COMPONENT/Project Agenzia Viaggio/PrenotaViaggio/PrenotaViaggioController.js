227bd59d418c096e50023c0e42c231a8b4b8f222
Christian Niro <91950577+chry2512@users.noreply.github.com>
2023-12-06 19:15:52 +0100

    Update PrenotaViaggioController.js

--- AURA COMPONENT/Project Agenzia Viaggio/PrenotaViaggio/PrenotaViaggioController.js
+++ AURA COMPONENT/Project Agenzia Viaggio/PrenotaViaggio/PrenotaViaggioController.js
@@ -15,27 +15,17 @@
 
     onClear : function(component, event, helper) {
 
-
-        var vgg = event.getSource().get("v.name");
-
+         var vgg = event.getSource().get("v.name");
         
-        
-        
-
-         
-
          helper.UpdateVgg(component, vgg);
 
          window.setTimeout(function(){ $A.get('e.force:refreshView').fire(); }, 1); 
          window.location.href = "/lightning/n/trivago2";
          
 
-
      },
 
     CreateAccounts : function(component, event, helper) {
-        
-       
         helper.NewAccounts(component);
     }
 
